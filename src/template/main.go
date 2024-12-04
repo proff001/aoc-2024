5a9cce46e72c49c0c52c2336061f12cbbb8b4fa5
Thomas Meller Hole <thommhole@gmail.com>
2024-12-04 22:43:09 +0100

    Updated template to include file import and result print

--- src/template/main.go
+++ src/template/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	file, _ := os.Open("src/day3/input.txt");
+	defer file.Close();
+
+	r := bufio.NewReader(file);
+	resultA := 0;
+	resultB := 0;
+
+	for {
+		lineBytes, _, err := r.ReadLine();
+
+		if (err != nil) {
+			break;
+		}		
+	}
+
+	fmt.Printf("Result A: %d\n", resultA);
+	fmt.Printf("Result B: %d\n", resultB);
 }
